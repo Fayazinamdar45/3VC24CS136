b1f05ef3228d49df0b5861fe6a1fadb36e0ba4b4
Fayazinamdar45 <fayazinamdar45@gmail.com>
2025-12-30 15:44:38 +0530

    Revert "I am in lab 8 main branch"
    This reverts commit 085529a65de4743c35f918a0bf2fdf245d2ab2fa.

---
+++ README.md
@@ -0,0 +1,3 @@
+# 3VC24CS136
+
+This is a git repository
---
+++ add2.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+void main(){
+    int a,b;
+    printf("Enter a number: );
+    scanf("%d",&a);
+    printf("Enter a number: );
+    scanf("%d",&b);
+    return 0;
+}
